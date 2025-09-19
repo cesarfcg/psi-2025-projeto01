e1024e07ff90d8af233c92fa177a39a7bf7f9032
Fernando César <fernandocesar4141@gmail.com>
2025-09-18 23:39:04 -0300

    mudando as nomeclaturas dos models no admin e sobrescrevendo  metodo utilizando "__str__".

--- website/models.py
+++ website/models.py
@@ -19,6 +19,10 @@ class Jogador(models.Model):
     local_nascimento = models.CharField( max_length=50)
     nacionalidade = models.CharField( max_length=50)
     imagem = models.ImageField(upload_to="jogadores/",blank=True)
+    def __str__(self):
+        return self.nome_camisa
+    class Meta:
+        verbose_name_plural = "Jogadores"
 class Jogo(models.Model):
     CAMPEONATOCHOICE = (("BRASILEIRÃO","Brasileirão"), ("LIBERTADORES","Libertadores"),)
 
@@ -29,6 +33,9 @@ class Jogo(models.Model):
     time_visitante = models.CharField() 
     placar_casa = models.IntegerField(blank=True)
     placar_visitante = models.IntegerField(blank=True)
+    def __str__(self):
+        return self.campeonato
+
 
 class ProximoJogo(models.Model):
     CAMPEONATOCHOICE = (("BRASILEIRÃO","Brasileirão"), ("LIBERTADORES","Libertadores"),)
@@ -38,21 +45,37 @@ class ProximoJogo(models.Model):
     estadio = models.CharField()
     time_casa = models.CharField() 
     time_visitante = models.CharField() 
-    
+    def __str__(self):
+        return self.campeonato
 
 class Noticia(models.Model):
     titulo = models.CharField( max_length=50)
     data = models.DateField()
     resumo = models.TextField()
     imagem = models.ImageField(upload_to="noticias/",blank=True)
-
+    def __str__(self):
+        return self.titulo
         
 class Introducao(models.Model):
     introducao = models.TextField()
+    def __str__(self):
+        return self.introducao
+    class Meta:
+        verbose_name_plural = "Introducao"
 class Sobre(models.Model):
     sobre = models.TextField()
+    def __str__(self):
+        return self.sobre
+    class Meta:
+        verbose_name_plural = "Sobre"
 class Autor(models.Model):
     nome = models.CharField(max_length=80)
     email = models.EmailField( max_length=254)
+    def __str__(self):
+        return self.nome
+    class Meta:
+        verbose_name_plural = "Autores"
 class Credito(models.Model):
     credito = models.TextField()
+    def __str__(self):
+        return self.credito
--- website/views.py
+++ website/views.py
@@ -3,64 +3,64 @@ from . models import Jogador,Jogo,Noticia,Introducao,ProximoJogo,Credito,Autor,S
 from datetime import date
 # Create your views here.
 
-ultimos_jogos = [
-    {
-        "data": "29/06/2025",
-        "campeonato": "Super Mundial de Clubes",
-        "estadio": "Hard Rock Stadium",
-        "time_casa": "Flamengo",
-        "placar_casa": 2,
-        "placar_visitante": 4,
-        "time_visitante": "FC Bayern München"
-    },
-    {
-        "data": "24/06/2025",
-        "campeonato": "Super Mundial de Clubes",
-        "estadio": "Camping World Stadium",
-        "time_casa": "Flamengo",
-        "placar_casa": 1,
-        "placar_visitante": 1,
-        "time_visitante": "Los Angeles FC"
-    }
-]
+# ultimos_jogos = [
+#     {
+#         "data": "29/06/2025",
+#         "campeonato": "Super Mundial de Clubes",
+#         "estadio": "Hard Rock Stadium",
+#         "time_casa": "Flamengo",
+#         "placar_casa": 2,
+#         "placar_visitante": 4,
+#         "time_visitante": "FC Bayern München"
+#     },
+#     {
+#         "data": "24/06/2025",
+#         "campeonato": "Super Mundial de Clubes",
+#         "estadio": "Camping World Stadium",
+#         "time_casa": "Flamengo",
+#         "placar_casa": 1,
+#         "placar_visitante": 1,
+#         "time_visitante": "Los Angeles FC"
+#     }
+# ]
 
-proximo_jogo = {
-    "data": "12/07/2025",
-    "campeonato": "Brasileirão",
-    "estadio": "Maracanã",
-    "time_casa": "Flamengo",
-    "time_visitante": "São Paulo"
-}
-noticias = [
-    {   
-        "id":1,
-        "titulo": "Flamengo anuncia novo reforço para 2025",
-        "data": "05/07/2025",
-        "resumo": "O clube confirmou a contratação do meia que promete reforçar o elenco para a próxima temporada.",
-        "imagem":"website/img/jorginhoanuncio.jpg",
+# proximo_jogo = {
+#     "data": "12/07/2025",
+#     "campeonato": "Brasileirão",
+#     "estadio": "Maracanã",
+#     "time_casa": "Flamengo",
+#     "time_visitante": "São Paulo"
+# }
+# noticias = [
+#     {   
+#         "id":1,
+#         "titulo": "Flamengo anuncia novo reforço para 2025",
+#         "data": "05/07/2025",
+#         "resumo": "O clube confirmou a contratação do meia que promete reforçar o elenco para a próxima temporada.",
+#         "imagem":"website/img/jorginhoanuncio.jpg",
 
-    },
-    {
-        "id":2,
-        "titulo": "Flamengo inicia semana de treinos visando o retorno do Brasileirão",
-        "data": "08/07/2025",
-        "resumo": "Mais Querido enfrenta o São Paulo no próximo sábado (12), às 16h30, no Maracanã",
-        "imagem":"website/img/reestreia.jpg",
-    },
-]
-introducao = {"introducao":"O Clube de Regatas do Flamengo, carinhosamente conhecido como Mengão, é uma das maiores instituições esportivas do Brasil e do mundo. Fundado em 17 de novembro de 1895, no Rio de Janeiro, o clube nasceu como uma agremiação de remo e, ao longo do tempo, se transformou em um gigante do futebol. Com uma história repleta de títulos e ídolos inesquecíveis, o Flamengo acumula conquistas nacionais e internacionais, como a Copa Libertadores da América e o Campeonato Brasileiro. A Nação Rubro-Negra, como é conhecida sua apaixonada torcida, é a maior do país e uma das mais fervorosas do planeta. Mais do que um clube, o Flamengo é um símbolo de paixão, raça e glória."}
-sobre = {
-    "info": "Este site foi desenvolvido com o objetivo de demonstrar conhecimentos em desenvolvimento web utilizando o framework Django. Ele apresenta informações organizadas, com funcionalidades como:"
-    }
-autores = [
-    {
-        "nome":"Fernando César Campos Gonzaga",
-        "email":"fernandocesar4141@gmail.com",
-    },
-]
-creditos = {"creditos":"As informações e imagens utilizadas no site são de fontes públicas e têm finalidade exclusivamente educacional."}
+#     },
+#     {
+#         "id":2,
+#         "titulo": "Flamengo inicia semana de treinos visando o retorno do Brasileirão",
+#         "data": "08/07/2025",
+#         "resumo": "Mais Querido enfrenta o São Paulo no próximo sábado (12), às 16h30, no Maracanã",
+#         "imagem":"website/img/reestreia.jpg",
+#     },
+# ]
+# introducao = {"introducao":"O Clube de Regatas do Flamengo, carinhosamente conhecido como Mengão, é uma das maiores instituições esportivas do Brasil e do mundo. Fundado em 17 de novembro de 1895, no Rio de Janeiro, o clube nasceu como uma agremiação de remo e, ao longo do tempo, se transformou em um gigante do futebol. Com uma história repleta de títulos e ídolos inesquecíveis, o Flamengo acumula conquistas nacionais e internacionais, como a Copa Libertadores da América e o Campeonato Brasileiro. A Nação Rubro-Negra, como é conhecida sua apaixonada torcida, é a maior do país e uma das mais fervorosas do planeta. Mais do que um clube, o Flamengo é um símbolo de paixão, raça e glória."}
+# sobre = {
+#     "info": "Este site foi desenvolvido com o objetivo de demonstrar conhecimentos em desenvolvimento web utilizando o framework Django. Ele apresenta informações organizadas, com funcionalidades como:"
+#     }
+# autores = [
+#     {
+#         "nome":"Fernando César Campos Gonzaga",
+#         "email":"fernandocesar4141@gmail.com",
+#     },
+# ]
+# creditos = {"creditos":"As informações e imagens utilizadas no site são de fontes públicas e têm finalidade exclusivamente educacional."}
 
-context = {"ultimos_jogos":ultimos_jogos,"proximo_jogo":proximo_jogo , "introducao":introducao,"noticias":noticias, "sobre":sobre, "autores":autores,"creditos":creditos}
+# context = {"ultimos_jogos":ultimos_jogos,"proximo_jogo":proximo_jogo , "introducao":introducao,"noticias":noticias, "sobre":sobre, "autores":autores,"creditos":creditos}
 
 def index(request):
     noticias = Noticia.objects.all()
